9d085c38aa740274f9c3aca396fbe98c2aa33044
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-11-30 20:53:13 -0800

    attempting to connect api to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const data = axios.get('http://localhost:5000/api/courses')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Application</h1>
+      <p>{data}</p>
     </div>
   );
 }
+
 
 export default App;
