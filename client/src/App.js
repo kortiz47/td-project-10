93aca1001fc9669eee9d7e3054909366cdbde1bc
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-11-30 23:49:31 -0800

    not found, forbidden, and error components created and routes functional

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,10 @@ import UpdateCourse from './components/UpdateCourse';
 import CourseDetail from './components/CourseDetail';
 import UserSignIn from './components/UserSignIn';
 import UserSignUp from './components/UserSignUp';
-
+//Error Components
+import UnhandledError from './components/UnhandledError';
+import Forbidden from './components/Forbidden';
+import NotFound from './components/NotFound';
 
 const App = () => {
   const [data, setData] = useState([]);
@@ -31,12 +34,15 @@ const App = () => {
       <Header />
       <Routes>
         <Route path='/' element={<Courses />} />
-        <Route path='/courses/create' element={<CreateCourse />}/>
-        <Route path='/courses/:id/update' element={<UpdateCourse />}/>
-        <Route path='/courses/:id' element={<CourseDetail />}/>
-        <Route path='/signin' element={<UserSignIn />}/>
-        <Route path='/signup' element={<UserSignUp />}/>
-        <Route path='/signout'/>
+        <Route path='/courses/create' element={<CreateCourse />} />
+        <Route path='/courses/:id/update' element={<UpdateCourse />} />
+        <Route path='/courses/:id' element={<CourseDetail />} />
+        <Route path='/signin' element={<UserSignIn />} />
+        <Route path='/signup' element={<UserSignUp />} />
+        <Route path='/signout' />
+        <Route path='/notfound' element={<NotFound />} />
+        <Route path='/forbidden' element={<Forbidden />} />
+        <Route path='/error' element={<UnhandledError />} />
       </Routes>
     </div>
   );
