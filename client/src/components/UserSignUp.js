591d825fa332af59b1136394e972dcd5c619c6f4
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-12-01 18:31:38 -0800

    user sign up component links cleaned up and needs submit functionality

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,24 +1,39 @@
+import { Link } from 'react-router-dom';
+import { useRef } from 'react';
+
 const UserSignUp = () =>{
+    const fname = useRef();
+    const lname = useRef();
+    const email = useRef();
+    const password = useRef();
+
+    const handleSubmit = () =>{
+        console.log(`${fname}, ${lname}, ${email}, ${password}`)
+    }
+
     return(
         <main>
             <div className="form--centered">
                 <h2>Sign Up</h2>
                 
                 <form>
-                    <label for="firstName">First Name</label>
-                    <input id="firstName" name="firstName" type="text" value="" />
-                    <label for="lastName">Last Name</label>
-                    <input id="lastName" name="lastName" type="text" value="" />
-                    <label for="emailAddress">Email Address</label>
-                    <input id="emailAddress" name="emailAddress" type="email" value="" />
-                    <label for="password">Password</label>
-                    <input id="password" name="password" type="password" value="" />
-                    <button className="button" type="submit">Sign Up</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
+                    <label htmlFor="firstName">First Name</label>
+                    <input id="firstName" name="firstName" type="text" required ref={fname}/>
+                    <label htmlFor="lastName">Last Name</label>
+                    <input id="lastName" name="lastName" type="text" required ref={lname} />
+                    <label htmlFor="emailAddress">Email Address</label>
+                    <input id="emailAddress" name="emailAddress" type="email" required ref={email} />
+                    <label htmlFor="password">Password</label>
+                    <input id="password" name="password" type="password" required ref={password}/>
+                    <button className="button" type="submit" onSubmit={handleSubmit}>Sign Up</button>
+                    <Link to='/'><button className="button button-secondary">Cancel</button></Link>
                 </form>
-                <p>Already have a user account? Click here to <a href="sign-in.html">sign in</a>!</p>
+                <p>Already have a user account? Click here to <Link to="/signin">sign in</Link>!</p>
             </div>
         </main>
     )
 }
 
 export default UserSignUp;
+
+//onclick="event.preventDefault(); location.href='index.html';"
