0d10cdd7bbaf02ab4b47ad3d92ee209b25518065
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-12-01 18:10:08 -0800

    userSignIn component links cleaned and added useRef

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,16 +1,23 @@
 import { Link } from "react-router-dom";
+import { useRef } from "react";
 
 const UserSignIn = () => {
+    const userPassword = useRef();
+    const userEmail = useRef();
+
     return (
         <main>
             <div className="form--centered">
                 <h2>Sign In</h2>
                 <form>
                     <label for="emailAddress">Email Address</label>
-                    <input id="emailAddress" name="emailAddress" type="email" value="" />
+                    <input id="emailAddress" name="emailAddress" type="email" required ref={userEmail}/>
                     <label for="password">Password</label>
-                    <input id="password" name="password" type="password" value="" />
-                    <button className="button" type="submit">Sign In</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='index.html';">Cancel</button>
+                    <input id="password" name="password" type="password" required ref={userPassword}/>
+                    <button className="button" type="submit">Sign In</button>
+                    <Link to='/'>
+                        <button className="button button-secondary">Cancel</button>
+                    </Link>
                 </form>
                 <p>Don't have a user account? Click here to <Link to="/signup">sign up</Link>!</p>
             </div>
@@ -19,3 +26,11 @@ const UserSignIn = () => {
 }
 
 export default UserSignIn;
+
+//onclick="event.preventDefault()"
+
+/** TODO */
+
+//Need to make sure a user can log into the form and be able to submit it/create a post request to the api
+
+//Also need to fix the link a user is directed to after logging into the app
