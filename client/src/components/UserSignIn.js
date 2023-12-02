dbe7383f491c614bacdfc90e5168e9075f5a80f1
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-12-01 18:21:24 -0800

    usersignin component requires sign in functionality

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -4,15 +4,20 @@ import { useRef } from "react";
 const UserSignIn = () => {
     const userPassword = useRef();
     const userEmail = useRef();
+    
+    const handleSubmit = () =>{
+        console.log("password: " + userPassword.current.value);
+        console.log("email: " + userEmail.current.value);
+    }
 
     return (
         <main>
             <div className="form--centered">
                 <h2>Sign In</h2>
                 <form>
-                    <label for="emailAddress">Email Address</label>
-                    <input id="emailAddress" name="emailAddress" type="email" required ref={userEmail}/>
-                    <label for="password">Password</label>
+                    <label htmlFor="emailAddress">Email Address</label>
+                    <input id="emailAddress" name="emailAddress" type="email" required ref={userEmail} onChange={handleSubmit}/>
+                    <label htmlFor="password">Password</label>
                     <input id="password" name="password" type="password" required ref={userPassword}/>
                     <button className="button" type="submit">Sign In</button>
                     <Link to='/'>
